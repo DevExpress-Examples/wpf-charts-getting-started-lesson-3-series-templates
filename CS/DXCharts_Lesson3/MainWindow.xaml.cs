3f9c58f06064a120cf6c7e792694e79af66b6e29
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXCharts_Lesson3/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace DXCharts_Lesson3 {
+    
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            chartControl1.DataSource =
+           new gspDataSetTableAdapters.GSPTableAdapter().GetData();
+        }
+    }
+}
