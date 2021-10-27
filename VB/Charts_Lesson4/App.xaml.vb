30df750a31ec1ae2f6322b9587b31b9d30250ffb
DevExpressExampleBot <fakeEmail>
2021-10-27 10:37:58 +0300

    autogenerate VB

---
+++ VB/Charts_Lesson4/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace Charts_Lesson4
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
---
+++ VB/Charts_Lesson4/MainWindow.xaml.vb
@@ -0,0 +1,16 @@
+Imports System.Windows
+Imports System.Windows.Controls
+
+Namespace Charts_Lesson4
+
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
